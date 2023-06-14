07ad676a14cabcd33ab5b0258d5f794f2a3bda63
timur <timur@MR-ROBOT>
2023-06-15 02:21:23 +0300

    create partial view and view components

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz").MinimumLength(50).WithMessage("Lütfen en az 50 karakter giriniz.").MaximumLength(1500).WithMessage("Lütfen Daha az karakter giriniz.");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Resim Seçiniz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz");
+        }
+    }
+}
