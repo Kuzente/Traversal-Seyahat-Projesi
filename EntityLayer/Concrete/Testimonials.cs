092f8e9c176224418aa00ba13bd7980e006cd993
timur <timur@MR-ROBOT>
2023-06-14 19:49:05 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Testimonials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonials:BaseEntity
+    {
+        public string Client { get; set; }
+        public string Comment { get; set; }
+        public string ClientImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
