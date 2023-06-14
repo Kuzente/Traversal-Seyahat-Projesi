f94ae46e794aa305586eda69bda8376f25da9a51
timur <timur@MR-ROBOT>
2023-06-15 01:19:36 +0300

    Business Layer Abstract / Concrete Create

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -18,6 +18,12 @@ namespace DataAccessLayer.Repository
             context.SaveChanges();
         }
 
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
         public List<T> GetList()
         {
             using var context = new Context();
